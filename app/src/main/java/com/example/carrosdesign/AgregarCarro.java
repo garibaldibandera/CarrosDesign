595f5b1aa06d8b87e6cc79dea7cd118ff21fed8c
Jose Bandera <jbandera@Defensoria.gov.co>
2020-06-13 16:20:30 -0500

    Validacion de placa existente ok

--- app/src/main/java/com/example/carrosdesign/AgregarCarro.java
+++ app/src/main/java/com/example/carrosdesign/AgregarCarro.java
@@ -3,6 +3,7 @@ package com.example.carrosdesign;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.icu.util.Calendar;
@@ -13,8 +14,14 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -28,11 +35,30 @@ public class AgregarCarro extends AppCompatActivity {
     private StorageReference storageReference;
     private Uri uri;
     private ImageView foto;
+    private static DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
+
+    private String error_placa;
+    private String error_marca;
+    private String error_modelo;
+    private String error_modelo_ano;
+    private String error_color;
+    private String error_precio;
+
+    Calendar cal= Calendar.getInstance();
+    int year= cal.get(Calendar.YEAR);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_carro);
+
+        error_placa=getResources().getString(R.string.error_placa);
+        error_marca=getResources().getString(R.string.error_marca);
+        error_modelo=getResources().getString(R.string.error_modelo);
+        error_modelo_ano=getResources().getString(R.string.error_modelo_ano);
+        error_color=getResources().getString(R.string.error_color);
+        error_precio=getResources().getString(R.string.error_precio);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -52,63 +78,68 @@ public class AgregarCarro extends AppCompatActivity {
         fotos.add(R.drawable.carro7);
 
         storageReference= FirebaseStorage.getInstance().getReference();
+
+     /*   placa.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    Toast.makeText(AgregarCarro.this, "Focus Lose", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(AgregarCarro.this, "Get Focus", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });*/
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void guardar(View v){
-        String plac, marc, model, colo, preci, id;
-        int foto;
-        Carro carro;
-        InputMethodManager imp=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        if (validar()) {
-            plac = placa.getText().toString();
-            marc = marca.getText().toString();
-            model = modelo.getText().toString();
-            colo = color.getText().toString();
-            preci = precio.getText().toString();
-            foto = foto_aleatoria();
-            id = Datos.getId();
-            carro = new Carro(plac, marc, model, colo, preci, foto, id);
-            carro.guardar();
-            subir_foto(id, foto);
-            limpiar();
-            imp.hideSoftInputFromWindow(placa.getWindowToken(), 0);
-            Snackbar.make(v, getString(R.string.mensaje_guardado_correcto), Snackbar.LENGTH_LONG).show();
+        if(placa.getText().toString().isEmpty()){
+            placa.setError(error_placa);
+        }else {
+            validarplaca(placa.getText().toString());
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public boolean validar(){
-        String error_placa, error_marca, error_modelo, error_modelo_ano, error_color, error_precio;
-        error_placa=getResources().getString(R.string.error_placa);
-        error_marca=getResources().getString(R.string.error_marca);
-        error_modelo=getResources().getString(R.string.error_modelo);
-        error_modelo_ano=getResources().getString(R.string.error_modelo_ano);
-        error_color=getResources().getString(R.string.error_color);
-        error_precio=getResources().getString(R.string.error_precio);
-        Calendar cal= Calendar.getInstance();
-        int year= cal.get(Calendar.YEAR);
+    private boolean validarplaca(String toString) {
 
-        if(placa.getText().toString().isEmpty()){
-            placa.setError(error_placa);
-            return false;
-        }else if(marca.getText().toString().isEmpty()){
-            marca.setError(error_marca);
-            return false;
-        }else if(modelo.getText().toString().isEmpty()){
-            modelo.setError(error_modelo);
-            return false;
-        }else if(Double.parseDouble(modelo.getText().toString())> year+1){
-            modelo.setError(error_modelo_ano);
-            return false;
-        }else if(color.getText().toString().isEmpty()) {
-            color.setError(error_color);
-            return false;
-        }else if(precio.getText().toString().isEmpty()) {
-            precio.setError(error_precio);
-            return false;
-        }
-        return true;
+        databaseReference.child("Carros").orderByChild("placa").equalTo(toString).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Map of all products which has city equal to mCurrentUserCity
+                        Carro carro = dataSnapshot.getValue(Carro.class);
+                        if (carro == null) {
+                            if(marca.getText().toString().isEmpty()){
+                                marca.setError(error_marca);
+                            }else if(modelo.getText().toString().isEmpty()){
+                                modelo.setError(error_modelo);
+                            }else if(Double.parseDouble(modelo.getText().toString())> year+1){
+                                modelo.setError(error_modelo_ano);
+                            }else if(color.getText().toString().isEmpty()) {
+                                color.setError(error_color);
+                            }else if(precio.getText().toString().isEmpty()) {
+                                precio.setError(error_precio);
+                            }else {
+                                String id = Datos.getId();
+                                carro = new Carro(placa.getText().toString(), marca.getText().toString(), modelo.getText().toString(), color.getText().toString(), precio.getText().toString(), foto_aleatoria(), id);
+                                carro.guardar();
+                                subir_foto(id, foto_aleatoria());
+                                limpiar();
+                                hideKeyboard(AgregarCarro.this);
+                                Toast.makeText(AgregarCarro.this, getString(R.string.mensaje_guardado_correcto), Toast.LENGTH_SHORT).show();
+                            }
+                        }else {
+                            Toast.makeText(AgregarCarro.this, getString(R.string.error_placa_existente), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+        return false;
     }
 
     public void subir_foto (String id, int foto){
@@ -137,8 +168,15 @@ public class AgregarCarro extends AppCompatActivity {
     }
 
     public void onBackPressed(){
-        finish();
         Intent i= new Intent(AgregarCarro.this, MainActivity.class);
         startActivity(i);
+    }
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = activity.getCurrentFocus();
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
  }
